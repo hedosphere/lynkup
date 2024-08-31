54e067f6782c67e35af319e21ba12535e5b3f1ba
Ogbo <109304551+hedosphere@users.noreply.github.com>
2024-08-31 05:19:46 +0100

    update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!, for vercel not again");
+  res.send("Hello World!, for vercel ");
 });
 
 app.listen(port, () => {
