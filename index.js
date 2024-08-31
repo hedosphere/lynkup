17dd6f9209dce0174694f083b13b3b39159ebaa9
Ogbo <109304551+hedosphere@users.noreply.github.com>
2024-08-31 05:07:04 +0100

    build vercel

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!, for vercel not again");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
